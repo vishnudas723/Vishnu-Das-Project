68494898dd9293e45ef84c70490a8a4ea1599cbe
Vishnu Das <vishnudas723@gmail.com>
2022-03-22 02:06:04 +0530

    Update log.js

--- log.js
+++ log.js
@@ -16,13 +16,13 @@ function validate(){
 let password = document.getElementById("exampleInputPassword1");
 let error1 = document.getElementById("passerror");
 
-function validate(inputtxt) {
+function validate() {
     let minNumberofChars = 8;
     let maxNumberofChars = 16;
     //let regexp1  = /^[a-zA-Z0-9!@#$%^&*]{8,16}$/
     //str.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
     var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    if(minNumberofChars<8 || maxNumberofChars>16 && inputtxt.value.match(re)){
+    if(minNumberofChars<8 || maxNumberofChars>16 && regexp.test(password.value)){
         error1.innerHTML = "Valid";
         error1.style.color ="Blue";
         return true;
