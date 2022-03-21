9485c7c6e016e9dd2b8f310fdcc94fcf1dc0f64a
Vishnu Das <vishnudas723@gmail.com>
2022-03-22 00:22:50 +0530

    commit2

---
+++ log.js
@@ -0,0 +1,17 @@
+let email = document.getElementById("exampleInputEmail1");
+let error = document.getElementById("error");
+function validate(){
+    let regexp = /^([A-Za-z0-9\.-]+)@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(regexp.test(email.value)){
+        error.innerHTML = "Valid";
+        error.style.color ="green";
+        return true;
+    }
+    else{
+        error.innerHTML = "Invalid";
+        error.style.color ="red";
+        return false;
+    }
+}
+
+
