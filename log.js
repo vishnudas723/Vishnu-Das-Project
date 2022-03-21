1939f878ae4b5accfdb259c24458d66b89944b42
Vishnu Das <vishnudas723@gmail.com>
2022-03-22 02:29:52 +0530

    Update log.js

--- log.js
+++ log.js
@@ -13,23 +13,7 @@ function validate(){
         return false;
     }
 }
-    let password = document.getElementById("exampleInputPassword1");
-    let error1 = document.getElementById("passerror");
-    function validate(){
-    let minNumberofChars = 8;
-    let maxNumberofChars = 16;
-    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    if(minNumberofChars<8 || maxNumberofChars>16 && regexp.test(password.value)){
-        error1.innerHTML = "Valid";
-        error1.style.color ="Blue";
-        return true;
-           }
-    else{
-        error1.innerHTML = "Invalid Password characters";
-        error1.style.color ="red";
-        return false; 
-        
-    }
-    }
+
+
 
 
