8837afd53cb3f6a022d6d84f068823e7e34ca97c
Vishnu Das <vishnudas723@gmail.com>
2022-03-22 02:08:41 +0530

    Update log.js

--- log.js
+++ log.js
@@ -1,5 +1,7 @@
 let email = document.getElementById("exampleInputEmail1");
 let error = document.getElementById("error");
+let password = document.getElementById("exampleInputPassword1");
+let error1 = document.getElementById("passerror");
 function validate(){
     let regexp = /^([A-Za-z0-9\.-]+)@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
     if(regexp.test(email.value)){
@@ -12,15 +14,9 @@ function validate(){
         error.style.color ="red";
         return false;
     }
-}
-let password = document.getElementById("exampleInputPassword1");
-let error1 = document.getElementById("passerror");
 
-function validate() {
     let minNumberofChars = 8;
     let maxNumberofChars = 16;
-    //let regexp1  = /^[a-zA-Z0-9!@#$%^&*]{8,16}$/
-    //str.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
     var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
     if(minNumberofChars<8 || maxNumberofChars>16 && regexp.test(password.value)){
         error1.innerHTML = "Valid";
@@ -34,22 +30,4 @@ function validate() {
         
     }
     }
-//function checkPassword(str)
- // {
-    // at least one number, one lowercase and one uppercase letter
-    // at least six characters
-  //  var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    //return re.test(str);
-   // if(re.test(str)){
-   //     error1.innerHTML = "Valid";
-   //     error1.style.color ="Blue";
-   //     return true;
-   // }
- //   else{
- //       error1.innerHTML = "Password Must contain atleast one Uppercase and Lowercase Character";
-   //     error1.style.color ="red";
-   //     return false;  
-  //  }
- //   }
-  //
 
