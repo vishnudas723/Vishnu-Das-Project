5f273553a2d0f594ac925b429f44830716cb7c3b
Vishnu Das <vishnudas723@gmail.com>
2022-03-22 13:10:56 +0530

    Update log.js

--- log.js
+++ log.js
@@ -1,48 +1,19 @@
 let email = document.getElementById("exampleInputEmail1");
-let password = document.getElementById("exampleInputPassword1");
 let error = document.getElementById("error");
-let error1 = document.getElementById("passerror");
 function validate(){
-    let regexp = /^([A-Za-z0-9\.-]+)@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
-    let minNumberofChars = 8;
-    let maxNumberofChars = 16;
-    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    if(regexp.test(email.value) || email.value==''){
+    let regexp = /^([A-Za-z0-9\.-]+)@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(regexp.test(email.value)){
         error.innerHTML = "Valid";
         error.style.color ="Blue";
         return true;
     }
-    else if(minNumberofChars<8 || maxNumberofChars>16 && regexp.test(password.value)){
-        error1.innerHTML = "Valid";
-        error1.style.color ="Blue";
-        return true;
-    }
     else{
-        error.innerHTML = "Invalid Mail ID";
+        error.innerHTML = "Invalid Email ID";
         error.style.color ="red";
-        return false; 
-        
+        return false;
     }
-   
 }
 
-/*let password = document.getElementById("exampleInputPassword1");
-let error1 = document.getElementById("passerror");
-function validate(){
-let minNumberofChars = 8;
-let maxNumberofChars = 16;
-var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-if(minNumberofChars<8 || maxNumberofChars>16 && regexp.test(password.value)){
-    error1.innerHTML = "Valid";
-    error1.style.color ="Blue";
-        return true;
-       }
-else{
-    error1.innerHTML = "Invalid Password characters";
-    error1.style.color ="red";
-    return false; 
-    
-}
-}*\
+
 
 
