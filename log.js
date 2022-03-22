7da16ccccff168b72e4de837679fd07c496cd93e
Vishnu Das <vishnudas723@gmail.com>
2022-03-22 12:07:16 +0530

    Update log.js

--- log.js
+++ log.js
@@ -15,19 +15,19 @@ function validate(){
 }
 
 let password = document.getElementById("exampleInputPassword1");
-let error = document.getElementById("passerror");
+let error1 = document.getElementById("passerror");
 function validate(){
 let minNumberofChars = 8;
 let maxNumberofChars = 16;
 var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
 if(minNumberofChars<8 || maxNumberofChars>16 && regexp.test(password.value)){
-    error.innerHTML = "Valid";
-    error.style.color ="Blue";
+    error1.innerHTML = "Valid";
+    error1.style.color ="Blue";
     return true;
        }
 else{
-    error.innerHTML = "Invalid Password characters";
-    error.style.color ="red";
+    error1.innerHTML = "Invalid Password characters";
+    error1.style.color ="red";
     return false; 
     
 }
