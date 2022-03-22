d185cb00753315a51129699cf591cc56eed840c3
Vishnu Das <vishnudas723@gmail.com>
2022-03-22 12:03:11 +0530

    Update log.js

--- log.js
+++ log.js
@@ -1,19 +1,22 @@
-let email = document.getElementById("exampleInputEmail1");
-let error = document.getElementById("error");
+
+
+let password = document.getElementById("exampleInputPassword1");
+let error1 = document.getElementById("passerror");
 function validate(){
-    let regexp = /^([A-Za-z0-9\.-]+)@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-    if(regexp.test(email.value)){
-        error.innerHTML = "Valid";
-        error.style.color ="Blue";
-        return true;
-    }
-    else{
-        error.innerHTML = "Invalid Email ID";
-        error.style.color ="red";
-        return false;
-    }
+let minNumberofChars = 8;
+let maxNumberofChars = 16;
+var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+if(minNumberofChars<8 || maxNumberofChars>16 && regexp.test(password.value)){
+    error1.innerHTML = "Valid";
+    error1.style.color ="Blue";
+    return true;
+       }
+else{
+    error1.innerHTML = "Invalid Password characters";
+    error1.style.color ="red";
+    return false; 
+    
 }
-
-
+}
 
 
