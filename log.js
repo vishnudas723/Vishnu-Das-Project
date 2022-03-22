923a99d8e35fa395e5d6c13256e237b3559ab7c2
Vishnu Das <vishnudas723@gmail.com>
2022-03-22 12:06:12 +0530

    Update log.js

--- log.js
+++ log.js
@@ -1,4 +1,18 @@
-
+let email = document.getElementById("exampleInputEmail1");
+let error = document.getElementById("error");
+function validate(){
+    let regexp = /^([A-Za-z0-9\.-]+)@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(regexp.test(email.value)){
+        error.innerHTML = "Valid";
+        error.style.color ="Blue";
+        return true;
+    }
+    else{
+        error.innerHTML = "Invalid Email ID";
+        error.style.color ="red";
+        return false;
+    }
+}
 
 let password = document.getElementById("exampleInputPassword1");
 let error = document.getElementById("passerror");
