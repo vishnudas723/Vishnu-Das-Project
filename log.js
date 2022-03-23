20e93b556f69cc6c5e2b6e3fa3f0a07e5449af4f
Vishnu Das <vishnudas723@gmail.com>
2022-03-24 00:39:51 +0530

    Update log.js

--- log.js
+++ log.js
@@ -35,8 +35,8 @@ form.onsubmit = (e)=>{
   function checkPass(){ //checkPass function
     let minNumberofChars = 6;
     let maxNumberofChars = 16;
-    let regularExpression  = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    if(pInput.value == "" ||  minNumberofChars < 8 || maxNumberofChars > 16 && !pInput.value.match(regularExpression)){ //if pass is empty then add error and remove valid class
+    let pattern  = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if(pInput.value == "" ||  minNumberofChars < 8 || maxNumberofChars > 16 && !pInput.value.match(pattern)){ //if pass is empty then add error and remove valid class
       pField.classList.add("error");
       pField.classList.remove("valid");
     }else{ //if pass is empty then remove error and add valid class
