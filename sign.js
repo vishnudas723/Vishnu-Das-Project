51687bc1139acbcfadb0b7077c058f4946a164c6
Vishnu Das <vishnudas723@gmail.com>
2022-03-25 00:07:16 +0530

    c

--- sign.js
+++ sign.js
@@ -1,191 +1,53 @@
-// First Name Validation
-var firstName= document.getElementById("firstName");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const phoneNumber = document.getElementById("phoneNumber");
+const gender = document.registration;
+const language = document.getElementById("language");
+const zipcode = document.getElementById("zipcode");
 
-var firstNameValidation=function(){
-
-   firstNameValue=firstName.value.trim(); 
-   validFirstName=/^[A-Za-z]+$/;
-   firstNameErr=document.getElementById('first-name-err');
-
-   if(firstNameValue=="")
-   {
-    firstNameErr.innerHTML="First Name is required";
-
-   }else if(!validFirstName.test(firstNameValue)){
-     firstNameErr.innerHTML="First Name must be only string without white spaces";
-   }else{
-     firstNameErr.innerHTML="";
-     return true;
-    
-   }
-}
-
-firstName.oninput=function(){
-   
-   firstNameValidation();
-}
-
-  // Last Name Validation
-  var lastName= document.getElementById("lastName");
-
-  var lastNameValidation= function(){
-
-   lastNameValue=lastName.value.trim(); 
-   validLastName=/^[A-Za-z]+$/;
-   lastNameErr=document.getElementById('last-name-err');
-
-   if(lastNameValue=="")
-   {
-    lastNameErr.innerHTML="Last Name is required";
-
-   }else if(!validLastName.test(lastNameValue)){
-     lastNameErr.innerHTML="Last Name must be only string without white spaces";
-   }else{
-     lastNameErr.innerHTML="";
-     return true;
-   }
-  }
-
-lastName.oninput=function(){
-
-   lastNameValidation();
-}
-
- // Email Address Validation
- var emailAddress= document.getElementById("emailAddress");;
- var emailAddressValidation= function(){
-
-  emailAddressValue=emailAddress.value.trim(); 
-   validEmailAddress=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-   emailAddressErr=document.getElementById('email-err');
-
-   if(emailAddressValue=="")
-   {
-    emailAddressErr.innerHTML="Email Address is required";
-
-   }else if(!validEmailAddress.test(emailAddressValue)){
-     emailAddressErr.innerHTML="Email Addre must be in valid formate with @ symbol";
-   }else{
-     emailAddressErr.innerHTML="";
-     return true;
-   }
-
- }
-
-emailAddress.oninput=function(){
-
-   emailAddressValidation();
-}
-
- // Mobile Number Validation
- var mobileNumber= document.getElementById("mobileNumber");
-
- var mobileNumberValidation = function(){
-
-   mobileNumberValue=mobileNumber.value.trim(); 
-   validMobileNumber=/^[0-9]*$/;
-   mobileNumberErr=document.getElementById('mobile-number-err');
-
-   if(mobileNumberValue=="")
-   {
-    mobileNumberErr.innerHTML="Mobile Number is required";
-
-   }else if(!validMobileNumber.test(mobileNumberValue)){
-     mobileNumberErr.innerHTML="Mobile Number must be a number";
-   }else if(mobileNumberValue.length!=10){
-
-      mobileNumberErr.innerHTML="Mobile Number must have 10 digits";
-   }
-   else{
-     mobileNumberErr.innerHTML="";
-     return true;
-   }
-
- }
-mobileNumber.oninput=function(){
-
-   mobileNumberValidation();
-}
-
-
-// Password Validation
-var password= document.getElementById("password");;
-
-var passwordValidation = function(){
-
-  passwordValue=password.value.trim(); 
-   validPassword=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-   
-   passwordErr=document.getElementById('password-err');
-
-   if(passwordValue=="")
-   {
-    passwordErr.innerHTML="Password is required";
-   }else if(!validPassword.test(passwordValue)){
-     passwordErr.innerHTML="Password must have at least one Uppercase, lowercase, digit, special characters & 8 characters";
-   }
-   else{
-     passwordErr.innerHTML="";
-     return true;
-   }
-}
-
-password.oninput=function(){
-
-   passwordValidation();
-
- confirmPasswordValidation();
-   
-}
-
-// Confirm Password Validation
-var confirmPassword= document.getElementById("confirmPassword");;
-
-var confirmPasswordValidation=function(){
-   confirmPasswordValue=confirmPassword.value.trim(); 
-   
-   confirmPasswordErr=document.getElementById('confirm-password-err');
-   if(confirmPasswordValue==""){
-       confirmPasswordErr.innerHTML="Confirm Password is required";
-   }
-
-  else if(confirmPasswordValue!=password.value){
-     confirmPasswordErr.innerHTML="Confirm Password does not match";
-   }
-   else{
-     confirmPasswordErr.innerHTML="";
-     return true;
-   }
-}
-
-confirmPassword.oninput=function(){
-
- confirmPasswordValidation();
-   
-}
-
-// validation on submit
-
-
-document.getElementById("registrationForm").onsubmit=function(e){
-
+// function for form varification
+function formValidation() {
   
-  firstNameValidation();
-  lastNameValidation();
-  emailAddressValidation();
-  mobileNumberValidation();
-  passwordValidation();
-  confirmPasswordValidation();
-
-  if(firstNameValidation()==true && 
-    lastNameValidation()==true && 
-    emailAddressValidation() == true && 
-    mobileNumberValidation()==true && 
-    passwordValidation()==true && 
-    confirmPasswordValidation()==true){
-    return true;
-  }else{
+  // checking name length
+  if (name.value.length < 2 || name.value.length > 20) {
+    alert("Name length should be more than 2 and less than 21");
+    name.focus();
     return false;
   }
+  // checking email
+  if (email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    alert("Please enter a valid email!");
+    email.focus();
+    return false;
+  }
+  // checking password
+  if (!password.value.match(/^.{5,15}$/)) {
+    alert("Password length must be between 5-15 characters!");
+    password.focus();
+    return false;
+  }
+  // checking phone number
+  if (!phoneNumber.value.match(/^[1-9][0-9]{9}$/)) {
+    alert("Phone number must be 10 characters long number and first digit can't be 0!");
+    phoneNumber.focus();
+    return false;
+  }
+  // checking gender
+  if (gender.gender.value === "") {
+    alert("Please select your gender!");
+    return false;
+  }
+  // checking language
+  if (language.value === "") {
+    alert("Please select your language!")
+    return false;
+  }
+  // checking zip code
+  if (!zipcode.value.match(/^[0-9]{6}$/)) {
+    alert("Zip code must be 6 characters long number!");
+    zipcode.focus();
+    return false;
+  }
+  return true;
 }
-
