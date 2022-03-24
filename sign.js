72e19e3d810d8494d0af62cecc08bb03b58a29f5
Vishnu Das <vishnudas723@gmail.com>
2022-03-25 00:37:56 +0530

    C

--- sign.js
+++ sign.js
@@ -1,63 +1,50 @@
-body {
-  font-size: 16px;
-  background: #f9f9f9;
-  font-family: "Segoe UI", "Helvetica Neue", Arial, sans-serif;
-}
-h2 {
-  text-align: center;
-  text-decoration: underline;
-}
-form {
-  width: 300px;
-  background: #fff;
-  padding: 15px 40px 40px;
-  border: 1px solid #ccc;
-  margin: 50px auto 0;
-  border-radius: 5px;
-}
-label {
-  display: block;
-  margin-bottom: 5px
-}
-label i {
-  color: #999;
-  font-size: 80%;
-}
-input, select {
-  border: 1px solid #ccc;
-  padding: 10px;
-  display: block;
-  width: 100%;
-  box-sizing: border-box;
-  border-radius: 2px;
-}
-.row {
-  padding-bottom: 10px;
-}
-.form-inline {
-  border: 1px solid #ccc;
-  padding: 8px 10px 4px;
-  border-radius: 2px;
-}
-.form-inline label, .form-inline input {
-  display: inline-block;
-  width: auto;
-  padding-right: 15px;
-}
-.error {
-  color: red;
-  font-size: 90%;
-}
-input[type="submit"] {
-  font-size: 110%;
-  font-weight: 100;
-  background: #006dcc;
-  border-color: #016BC1;
-  box-shadow: 0 3px 0 #0165b6;
-  color: #fff;
-  margin-top: 10px;
-  cursor: pointer;
-}
-input[type="submit"]:hover {
-  background: #0165b6;
+const form = document.querySelector("form");
+eField = form.querySelector(".email"),
+eInput = eField.querySelector("input"),
+pField = form.querySelector(".password"),
+pInput = pField.querySelector("input");
+
+form.onsubmit = (e)=>{
+  e.preventDefault(); //preventing from form submitting
+  //if email and password is blank then add shake class in it else call specified function
+  (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
+  (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
+
+  setTimeout(()=>{ //remove shake class after 500ms
+    eField.classList.remove("shake");
+    pField.classList.remove("shake");
+  }, 500);
+
+  eInput.onkeyup = ()=>{checkEmail();} //calling checkEmail function on email input keyup
+  pInput.onkeyup = ()=>{checkPass();} //calling checkPassword function on pass input keyup
+
+  function checkEmail(){ //checkEmail function
+    let pattern = /^([A-Za-z0-9\.-]+)@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/; //pattern for validate email
+    if(!eInput.value.match(pattern)){ //if pattern not matched then add error and remove valid class
+      eField.classList.add("error");
+      eField.classList.remove("valid");
+      let errorTxt = eField.querySelector(".error-txt");
+      //if email value is not empty then show please enter valid email else show Email can't be blank
+      (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+    }else{ //if pattern matched then remove error and add valid class
+      eField.classList.remove("error");
+      eField.classList.add("valid");
+    }
+  }
+
+  function checkPass(){ //checkPass function
+    let pattern  = /^[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+            if(pInput.value == "" ||  pInput.value.length < 7 || pInput.value.length > 16){ //if pass is empty then add error and remove valid class
+      pField.classList.add("error");
+      pField.classList.remove("valid");
+    }else{ //if pass is empty then remove error and add valid class
+      pField.classList.remove("error");
+      pField.classList.add("valid");
+    }
+  }
+
+  
+  if(!eField.classList.contains("error") && !pField.classList.contains("error")){
+    window.location.href = form.getAttribute("action"); 
+  }
 }
